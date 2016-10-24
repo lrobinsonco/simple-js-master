90a61cb75aa1877563348dcd1c830d0d7245d50a
lrobinsonco <lrobinsonco@gmail.com>
2016-10-24 16:54:36 -0600

    day two project
    upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+
+
+
+// Use JS to add header text into 'main-header' without button click.
+document.getElementById("main-header").innerHTML = "<h1>JS Practice</h1>";
+
+
+
+// Use JS to add paragraph text into 'action1-message' with button click from 'action1-btn'. -->
+document.getElementById("action1-btn").addEventListener("click", function(){
+    document.getElementById("action1-message").innerHTML = "Action #1 Button was Clicked!";
+});
+
+
+
+// Use JS to add an h2 into 'heading2' with button click.
+// The HTML has been created, this is your chance to add JS. 
+
+
+
+
+
+// Create 3 examples of JS being added to the HTML page immediately. 
+// You'll need to create both the HTML and JS for this. 
+
+
+
+
+
+
+// Create 3 examples of JS being added to the HTML page based on button clicks. 
+// You'll need to create both the HTML and JS for this. 
+
